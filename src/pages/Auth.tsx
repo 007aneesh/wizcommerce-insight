b307b2cba6a8091a9e0cebe531c2d6fc62641b81
Aneesh Aggarwal <aneeshx000@gmail.com>
2025-12-04 11:47:59 +0530

    fix: remove signup

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -105,10 +105,11 @@ export default function Auth() {
         </div>
 
         <Tabs defaultValue="login" className="w-full">
-          <TabsList className="grid w-full grid-cols-2">
+          
+          {/* <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="login">Login</TabsTrigger>
             <TabsTrigger value="signup">Sign Up</TabsTrigger>
-          </TabsList>
+          </TabsList> */}
 
           <TabsContent value="login">
             <form onSubmit={handleLogin} className="space-y-4">
